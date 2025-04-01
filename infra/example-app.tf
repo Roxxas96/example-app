5a6f2b1b6582f1c063808e4f5bdd9a3328e10bf8
Alexandre Gomez <gomez.a.corneille@gmail.com>
2025-04-01 08:48:50 +0200

    infra(example-service): configure helm chart
    Signed-off-by: Alexandre Gomez <gomez.a.corneille@gmail.com>

---
+++ infra/example-app.tf
@@ -0,0 +1,22 @@
+resource "kubernetes_namespace_v1" "example-app" {
+  metadata {
+    name = "example-app"
+  }
+}
+
+variable "harbor_credentials" {
+  description = "Credentials for the homelab-kube robot on harbor"
+  sensitive   = true
+  type        = string
+}
+
+resource "kubernetes_secret_v1" "harbor_credentials" {
+  metadata {
+    name      = "harbor-credentials"
+    namespace = kubernetes_namespace_v1.example-app.metadata[0].name
+  }
+  type = "kubernetes.io/dockerconfigjson"
+  data = {
+    ".dockerconfigjson" = var.harbor_credentials
+  }
+}
