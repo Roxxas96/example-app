b4a7e4fb0a53588b8c4f5b02b929fc50489ffa1b
Alexandre Gomez <gomez.a.corneille@gmail.com>
2025-05-03 12:17:55 +0200

    infra: change from whole stack to just crds in test infra monitoring ns
    Signed-off-by: Alexandre Gomez <gomez.a.corneille@gmail.com>

--- infra/test/monitoring.tf
+++ infra/test/monitoring.tf
@@ -4,17 +4,17 @@ resource "kubernetes_namespace_v1" "kube_prometheus" {
   }
 }
 
-variable "kube_prometheus_chart_version" {
+variable "prometheus_crds_chart_version" {
   description = "Version for the kube-prometheus-stack chart"
   type        = string
-  default     = "70.4.1"
+  default     = "20.0.0"
 }
 
-resource "helm_release" "kube_prometheus" {
-  name      = "kube-prometheus"
+resource "helm_release" "prometheus_crds" {
+  name      = "prometheus-crds"
   namespace = kubernetes_namespace_v1.kube_prometheus.metadata[0].name
 
-  chart      = "kube-prometheus-stack"
+  chart      = "prometheus-operator-crds"
   repository = "https://prometheus-community.github.io/helm-charts"
-  version    = var.kube_prometheus_chart_version
+  version    = var.prometheus_crds_chart_version
 }
